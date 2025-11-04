3abe9ec754bed0869b0749fa1e9ea17913d1d1d4
IGMA-IGMA <i9296517650@yandex.ru>
2025-11-05 01:32:06 +0300

    8

--- help_script/auto_commit.py
+++ help_script/auto_commit.py
@@ -1,11 +1,11 @@
 import os
 
 
-def auto_commit(comment: str):
+def auto_commit():
     os.system("git add .")
-    os.system(f"git commit -a -m {comment}")
+    os.system(f"git commit -a -m {input("Коментарий к комиту: ")}")
     os.system("git push")
 
 
 if __name__ == "__main__":
-    auto_commit(input("Коментарий к комиту: "))
+    auto_commit()
